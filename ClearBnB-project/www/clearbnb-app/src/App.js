22287fc423aa4951d67ea193fbbadc64f8810735
oscargregorius <oscar.gregorius@gmail.com>
2021-03-03 16:24:03 +0100

    Added router for the whole page
    Co-Authored-By: Ali <72066097+ali94160@users.noreply.github.com>

--- ClearBnB-project/www/clearbnb-app/src/App.js
+++ ClearBnB-project/www/clearbnb-app/src/App.js
@@ -1,15 +1,29 @@
-import { useEffect } from 'react';
 import Nav from './components/Nav';
 import './style/App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Home from './routes/Home';
+import About from './routes/About';
+import Residences from './routes/Residences';
+import Login from './routes/Login';
+import Register from './routes/Register';
 
 
 function App() {
 
   return (
+    <Router>
     <div className="App">
       <Nav />
-      <div className="wrapper"></div>
+        <div className="wrapper"></div>
+      <Switch>
+          <Route path="/" exact component={Home} /> 
+          <Route path="/" exact component={About} />  
+          <Route path="/" exact component={Residences} />  
+          <Route path="/" exact component={Login} />  
+          <Route path="/" exact component={Register}/>  
+      </Switch>
     </div>
+    </Router>
   );
 }
 
--- ClearBnB-project/www/clearbnb-app/src/components/Nav.js
+++ ClearBnB-project/www/clearbnb-app/src/components/Nav.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import '../style/Nav.css';
+import {Link} from 'react-router-dom'
 
 const Nav = () => {
 
   return (
     <div className="nav-bar">
+      <Link to="/">
       <h4>ClearBnB</h4>
-      
+      </Link>
     </div>
   ); 
 }
