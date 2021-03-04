920a19a0c38deddf452b604327886b9042683ecd
oscargregorius <oscar.gregorius@gmail.com>
2021-03-04 12:41:56 +0100

    Created the User context
    Co-Authored-By: Ali <72066097+ali94160@users.noreply.github.com>

---
+++ ClearBnB-project/www/clearbnb-app/src/contexts/UserContextProvider.js
@@ -0,0 +1,12 @@
+import React, { useState, createContext } from 'react'
+
+export const UserContext = createContext();
+
+export const UserProvider = (props) => {
+
+  return (
+    <UserContext.Provider>
+      {props.children}
+    </UserContext.Provider>
+  );
+}
