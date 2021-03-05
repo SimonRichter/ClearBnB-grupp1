1b140b1579eb35234cb2e3346a61a8bf021de36c
Ali94160 <ali.al-mousawi@hotmail.com>
2021-03-05 15:08:18 +0100

    updated homepage
    styling with grid, new images and fixes

--- ClearBnB-project/www/src/routes/Home.js
+++ ClearBnB-project/www/src/routes/Home.js
@@ -2,8 +2,7 @@ import React from 'react';
 import '../style/Home.css';
 const Home = () => {
   return (
-    <div className="test">
-
+    <div className="homeWrapper">
       <div className="home">
       <img className="homeImage" src="https://i.postimg.cc/kXXSQpKb/bkpicture-1.png" alt="" />
 
@@ -12,14 +11,14 @@ const Home = () => {
         <button className="exploreBtn" >Explore the residences</button>
       </div>
       <div class="arrow bounce"></div>
-      
       </div>
-      <div className="adventureDiv">
-        <p>EXPLORE THE WORLD</p>
-        <div className="divImg1">
+        <p className="exploreText">EXPLORE THE WORLD</p>
+        <div className="divImgs">     
           <img src="https://cdn.shopify.com/s/files/1/1824/2601/files/photo_2020-10-12_10-45-26_480x480.jpg?v=1602524781" alt=""/>
+          <img src="https://cdn.shopify.com/s/files/1/1824/2601/files/photo_2020-10-12_10-45-26_480x480.jpg?v=1602524781" alt=""/>
+          <img src="https://cdn.shopify.com/s/files/1/1824/2601/files/photo_2020-10-12_10-45-26_480x480.jpg?v=1602524781" alt=""/>
+        
         </div>
-      </div>
     </div>
 
   );
