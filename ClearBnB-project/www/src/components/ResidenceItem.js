18f71078184e082a12a46a809594a0d89051f53c
oscargregorius <oscar.gregorius@gmail.com>
2021-03-05 11:33:34 +0100

    Made a history push to the detail page

--- ClearBnB-project/www/src/components/ResidenceItem.js
+++ ClearBnB-project/www/src/components/ResidenceItem.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 import '../style/ResidenceItem.css';
 
-const ResidenceItem = ({residence}) => {
+const ResidenceItem = ({ residence }) => {
+
+  const history = useHistory()
+
+
+  const goToDetailPage = () => {
+    history.push('/residence/' + residence._id)
+  }
+
   return (
-    <div className="residenceItem">
+    <div className="residenceItem" onClick={goToDetailPage}>
       <img src={residence.imageURLs[0]} alt="" />
       <h4>{ residence.title }</h4>
     </div>
