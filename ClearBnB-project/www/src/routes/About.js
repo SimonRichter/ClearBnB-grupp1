36d812e17e622be01f0ba79b35ab3bcaf1abe985
Jonte111 <jonathan.pe@telia.com>
2021-03-05 11:22:15 +0100

    Made About.js and About.css

--- ClearBnB-project/www/src/routes/About.js
+++ ClearBnB-project/www/src/routes/About.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import '../style/About.css'
 
 const About = () => {
   return (
     <div className="about">
-      <h1>About page</h1>
+      <div className="outerContainer">
+
+        <div className ="title">
+          <h1>About page</h1>
+        </div>
+
+        <div className="upperDescription">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta autem illo ullam sunt reprehenderit quisquam! Numquam eligendi recusandae natus repellat asperiores, excepturi adipisci veniam illo nesciunt cumque at ipsam a.
+        </div>
+
+        <div className="lowerDescription">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam minima assumenda, pariatur at ab illo architecto cupiditate sit iure, explicabo ullam asperiores dolorem reiciendis reprehenderit nesciunt similique eaque quas dolor?
+        </div>
+
+      </div>
+
     </div>
   );
 }
