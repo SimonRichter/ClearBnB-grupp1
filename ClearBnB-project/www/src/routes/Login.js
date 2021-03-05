60da3ad06dcd1de623572f7f574263947c32c520
oscargregorius <oscar.gregorius@gmail.com>
2021-03-05 15:07:41 +0100

    Css style for the login component

--- ClearBnB-project/www/src/routes/Login.js
+++ ClearBnB-project/www/src/routes/Login.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import '../style/Login.css'
 
 const Login = () => {
   return (  
     <div className="login">
-      <h1>Login</h1>
+      <div className="form-for-login">
+      <form>
+        <input required placeholder="Email.." type="email" />
+        <input required placeholder="Password.." type="password" />
+        <button>Login</button>
+      </form>
+      </div>
     </div>
   );
 }
