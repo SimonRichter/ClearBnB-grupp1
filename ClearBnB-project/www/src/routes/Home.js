5d7e2daf645fe7d6418f0087a384616aa635eef8
Ali94160 <ali.al-mousawi@hotmail.com>
2021-03-05 11:55:46 +0100

    Update homePage
    New bounching arrow to show the visitor that there is more info. Button and text to show the visitor to explore the residences.

--- ClearBnB-project/www/src/routes/Home.js
+++ ClearBnB-project/www/src/routes/Home.js
@@ -3,8 +3,19 @@ import '../style/Home.css';
 const Home = () => {
   return (
     <div className="home">
-      <img className="homeImage" src="https://i.postimg.cc/kXXSQpKb/bkpicture-1.png" alt=""/>
+      <img className="homeImage" src="https://i.postimg.cc/kXXSQpKb/bkpicture-1.png" alt="" />
+
+      <div className="startTravel">
+        <h4>Start Travel</h4>
+        <button>Explore the residences</button>
+      </div>
+
+      <div className="infoImages">
+        <p>Experience the adventures</p>
+      </div>
+      <div class="arrow bounce"></div>
     </div>
+
   );
 }
  
