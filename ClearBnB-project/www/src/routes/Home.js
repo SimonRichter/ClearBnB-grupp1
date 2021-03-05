cff21806a1d5d98f531814a30627e427233afc5d
Ali94160 <ali.al-mousawi@hotmail.com>
2021-03-05 13:53:47 +0100

    update homepage
    fixes

--- ClearBnB-project/www/src/routes/Home.js
+++ ClearBnB-project/www/src/routes/Home.js
@@ -2,15 +2,24 @@ import React from 'react';
 import '../style/Home.css';
 const Home = () => {
   return (
-    <div className="home">
+    <div className="test">
+
+      <div className="home">
       <img className="homeImage" src="https://i.postimg.cc/kXXSQpKb/bkpicture-1.png" alt="" />
 
       <div className="startTravel">
         <p>Start Travel</p>
         <button className="exploreBtn" >Explore the residences</button>
       </div>
-
       <div class="arrow bounce"></div>
+      
+      </div>
+      <div className="adventureDiv">
+        <p>EXPLORE THE WORLD</p>
+        <div className="divImg1">
+          <img src="https://cdn.shopify.com/s/files/1/1824/2601/files/photo_2020-10-12_10-45-26_480x480.jpg?v=1602524781" alt=""/>
+        </div>
+      </div>
     </div>
 
   );
