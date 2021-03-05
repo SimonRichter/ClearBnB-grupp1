8e4e433c9eb8a38b6602d436d417d84a3b3cb94d
oscargregorius <oscar.gregorius@gmail.com>
2021-03-05 09:07:42 +0100

    Connected the backend with frontend
    fetched all the users to the user context.
    
    Co-Authored-By: Jonte111 <72066499+Jonte111@users.noreply.github.com>
    Co-Authored-By: Simon Richter <simonmrichter@gmail.com>
    Co-Authored-By: Ali <72066097+ali94160@users.noreply.github.com>

--- ClearBnB-project/www/src/contexts/UserContextProvider.js
+++ ClearBnB-project/www/src/contexts/UserContextProvider.js
@@ -1,11 +1,23 @@
-import React, { useState, createContext } from 'react'
+import React, { useState, createContext, useEffect } from 'react'
 
 export const UserContext = createContext();
 
 export const UserProvider = (props) => {
 
+  
+  const fetchUsers = async() => {
+    let data = await fetch('/rest/users')
+    data = await data.json();
+    console.log(data);
+  }
+
+
+  useEffect(() => {
+    fetchUsers();
+  },[]);
+
   return (
-    <UserContext.Provider>
+    <UserContext.Provider value>
       {props.children}
     </UserContext.Provider>
   );
