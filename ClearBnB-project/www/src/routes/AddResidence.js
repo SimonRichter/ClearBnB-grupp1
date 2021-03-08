61d1e27c715702fb1a301590b4120c5c124d23b7
Ali94160 <ali.al-mousawi@hotmail.com>
2021-03-08 11:17:12 +0100

    updated: addResidence-view

--- ClearBnB-project/www/src/routes/AddResidence.js
+++ ClearBnB-project/www/src/routes/AddResidence.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import AddResidenceStyle from '../style/AddResidenceStyle.css';
+import React, { useState }from 'react';
+import '../style/AddResidenceStyle.css';
+
+
 
 const AddResidence = () => {
+
+  const [guests, setGuests] = useState(1);
+
+  const incGuestHandler = () => {
+      setGuests(guests + 1); 
+  }
+
+  const decGuestHanlder = () => {
+    if (guests <= 1) {
+      setGuests(1);
+    } else {
+      setGuests(guests-1);
+    }
+  }
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+  }
+
   return (
     <div className="addResWrapper">
+      <form onSubmit={submitHandler}>
       <h3>What type of recidense would you like to host?</h3>
-      <select>
-      <option className="optionBar" value="" disabled="disabled" selected="selected">Choose</option>
+      <select className="optionBar">
+      <option value="" disabled="disabled" selected="selected">Choose</option>
         <option value="1">House</option>
         <option value="2">Apartment</option>
         <option value="3">Cabin</option>
@@ -14,8 +36,13 @@ const AddResidence = () => {
         <option value="5">Mansion</option>
         <option value="6">Igloo</option>
       </select>
-      <form>
-      <label></label>
+     
+      
+        <div className="guestDiv">
+        <button className="incGuests" onClick={incGuestHandler}>＋</button>
+        <span className="numberOfGuests">Guests: {guests}</span>
+        <button className="decGuests" onClick={decGuestHanlder}>－</button>
+        </div>
 
       </form>
     </div>
