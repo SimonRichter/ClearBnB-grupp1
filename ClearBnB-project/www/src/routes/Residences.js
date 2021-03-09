fd89592f73e59314ec5d8bb0f6153fff8cc2ac42
oscargregorius <oscar.gregorius@gmail.com>
2021-03-09 13:42:25 +0100

    Added a search bar for country and city

--- ClearBnB-project/www/src/routes/Residences.js
+++ ClearBnB-project/www/src/routes/Residences.js
@@ -1,27 +1,43 @@
 import React, { useContext,useRef,useState } from 'react';
 import { ResidenceContext } from '../contexts/ResidenceContextProvider';
 import ResidenceList from '../components/ResidenceList'
+import '../style/Residences.css';
 
 const Residences = () => {
 
   const { residences } = useContext(ResidenceContext);
   const [filteredList, setFilteredList] = useState([...residences]);
-  const [search, setSearch] = useState(null);
+  const [searchCountry, setSearchCountry] = useState(null);
+  const [searchCity, setSearchCity] = useState(null);
 
-  const test = (e) => {
-    setSearch(e.target.value);
-    if (e.target.value === '') {
-      setFilteredList([...residences]);
+
+  const country = useRef();
+  const city = useRef();
+
+
+  const showAll = () => {
+    setFilteredList([...residences]);
+  }
+
+  const searchFor = (e) => {
+    e.preventDefault();
+    if (city.current.value !== '' && country.current.value !== '') {
+      const filter = residences.filter(r => r.country === country.current.value &&
+      r.city === city.current.value);
+      setFilteredList([...filter]);
       return;
     }
-    const filter = residences.filter(r => r.country.includes(search));
-    console.log(filter);
-    setFilteredList([...filter]);
   }
+ 
 
   return (  
     <div className="residences">
-      <input onChange={test} type="text" placeholder="Search by country.." />
+      <form onSubmit={searchFor}>
+        <input ref={country} type="text" placeholder="Search by country.." />
+        <input ref={city} type="text" placeholder="Search by city.." />
+        <button>Search</button>
+      </form>
+      <button onClick={showAll}>Show all</button>
       <ResidenceList residences={filteredList} />
     </div>
   );
