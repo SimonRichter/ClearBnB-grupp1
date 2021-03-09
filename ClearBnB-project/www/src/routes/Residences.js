0a03abf4a965cf4fa991eb662293f4a9a26c1ef7
oscargregorius <oscar.gregorius@gmail.com>
2021-03-09 11:57:08 +0100

    Added a search bar
    Input field for searching for countrys

--- ClearBnB-project/www/src/routes/Residences.js
+++ ClearBnB-project/www/src/routes/Residences.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useContext,useRef,useState } from 'react';
+import { ResidenceContext } from '../contexts/ResidenceContextProvider';
 import ResidenceList from '../components/ResidenceList'
 
 const Residences = () => {
+
+  const { residences } = useContext(ResidenceContext);
+  const [filteredList, setFilteredList] = useState([...residences]);
+  const [search, setSearch] = useState(null);
+
+  const test = (e) => {
+    setSearch(e.target.value);
+    if (e.target.value === '') {
+      setFilteredList([...residences]);
+      return;
+    }
+    const filter = residences.filter(r => r.country.includes(search));
+    console.log(filter);
+    setFilteredList([...filter]);
+  }
+
   return (  
     <div className="residences">
-      <ResidenceList/>
+      <input onChange={test} type="text" placeholder="Search by country.." />
+      <ResidenceList residences={filteredList} />
     </div>
   );
 }
