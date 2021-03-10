c9e4a40def31720c8fc31574e3e69230175c8469
oscargregorius <oscar.gregorius@gmail.com>
2021-03-10 13:17:41 +0100

    Some styling for the confirm "page"

--- ClearBnB-project/www/src/routes/ResidenceDetails.js
+++ ClearBnB-project/www/src/routes/ResidenceDetails.js
@@ -20,6 +20,7 @@ const ResidenceDetails = () => {
   const [endDate, setEndDate] = useState(null);
   const [totalPrice, setTotalPrice] = useState(null);
   const [unFilledFields, setunFilledFields] = useState(null);
+  const [showConfirmPage, setShowConfirmPage] = useState(false);
 
   const bookResidence = () => {
 
@@ -45,8 +46,8 @@ const ResidenceDetails = () => {
       bookedDays: allTheDaysBooked
     }
 
-    updateResidence(residence._id, bookedDaysObj);
-
+    //updateResidence(residence._id, bookedDaysObj);
+    setShowConfirmPage(true);
     const bookingObj = {
       startDate: startDate,
       endDate: endDate,
@@ -95,61 +96,72 @@ const ResidenceDetails = () => {
 
   return (
     <div className="residenceDetail">
-      <div className="images">
-      {residence.imageURLs.map((img) => {
-        return (<img key={img} src={img} alt=""/>)
-      })}
-      </div>
-      <div className="infoWrapper">
-        <div className="desc">
-        <h1>{residence.title}</h1>
-        <p><span>Country: </span>{residence.country}</p>
-        <p><span>City: </span>{residence.city}</p>
-        <p><span>Address: </span>{residence.address}</p>
-        <p><span>Type: </span>{residence.type}</p>
-        <p><span>Price per night: </span>{residence.price}€</p>
-        <p><span>Description: </span>{residence.description}</p>
+      {!showConfirmPage && <div className="inner">
+        <div className="images">
+          {residence.imageURLs.map((img) => {
+            return (<img key={img} src={img} alt="" />)
+          })}
         </div>
-        {features && <div className="features">
-          <p className={features.shower ? '' : 'dontExist'}><i className="material-icons">shower</i> Shower</p>
-          <p className={features.firstAidKit ? '' : 'dontExist'}><i className="material-icons">healing</i> first aid kit</p>
-          <p className={features.parking ? '' : 'dontExist'}><i className="material-icons">local_parking</i> parking</p>
-          <p className={features.stove ? '' : 'dontExist'}><i className="material-icons">kitchen</i> stove</p>
-          <p className={features.oven ? '' : 'dontExist'}><i className="material-icons">kitchen</i> oven</p>
-          <p className={features.microwave ? '' : 'dontExist'}><i className="material-icons">kitchen</i> microwave</p>
-          <p className={features.tv ? '' : 'dontExist'}><i className="material-icons">tv</i> TV</p>
-          <p className={features.coffeeMaker ? '' : 'dontExist'}><i className="material-icons">free_breakfast</i> Coffee maker</p>
-          <p className={features.wifi ? '' : 'dontExist'}><i className="material-icons">wifi</i> WiFi</p>
-          <p className={features.balcony ? '' : 'dontExist'}><i className="material-icons">straighten</i> Balcony</p>
-          <p className={features.iron ? '' : 'dontExist'}><i className="material-icons">scanner</i> Iron</p>
-          <p className={features.pool ? '' : 'dontExist'}><i className="material-icons">pool</i> Pool</p>
-          <p className={features.fridge ? '' : 'dontExist'}><i className="material-icons">kitchen</i> Fridge</p>
-          <p className={features.dishwasher ? '' : 'dontExist'}><i className="material-icons">kitchen</i> Dishwasher</p>
-        </div>}
-      </div>
-      <div className="dates">
-        <DatePicker className="startDate"
-          placeholderText="Arrival.."
-          selected={startDate}
-          onChange={date => setStartDate(date)}
-          minDate={residence.startDate * 1000}
-          maxDate={residence.endDate * 1000}
-          filterDate={filterForStartDate}
-          isClearable
-        />
-        <DatePicker className="endDate"
-          placeholderText="Departure.."
-          selected={endDate}
-          onChange={date => setEndDate(date)}
-          minDate={residence.startDate * 1000}
-          maxDate={residence.endDate * 1000}
-          filterDate={filterForEndDate}
-          isClearable
+        <div className="infoWrapper">
+          <div className="desc">
+            <h1>{residence.title}</h1>
+            <p><span>Country: </span>{residence.country}</p>
+            <p><span>City: </span>{residence.city}</p>
+            <p><span>Address: </span>{residence.address}</p>
+            <p><span>Type: </span>{residence.type}</p>
+            <p><span>Price per night: </span>{residence.price}€</p>
+            <p><span>Description: </span>{residence.description}</p>
+          </div>
+          {features && <div className="features">
+            <p className={features.shower ? '' : 'dontExist'}><i className="material-icons">shower</i> Shower</p>
+            <p className={features.firstAidKit ? '' : 'dontExist'}><i className="material-icons">healing</i> first aid kit</p>
+            <p className={features.parking ? '' : 'dontExist'}><i className="material-icons">local_parking</i> parking</p>
+            <p className={features.stove ? '' : 'dontExist'}><i className="material-icons">kitchen</i> stove</p>
+            <p className={features.oven ? '' : 'dontExist'}><i className="material-icons">kitchen</i> oven</p>
+            <p className={features.microwave ? '' : 'dontExist'}><i className="material-icons">kitchen</i> microwave</p>
+            <p className={features.tv ? '' : 'dontExist'}><i className="material-icons">tv</i> TV</p>
+            <p className={features.coffeeMaker ? '' : 'dontExist'}><i className="material-icons">free_breakfast</i> Coffee maker</p>
+            <p className={features.wifi ? '' : 'dontExist'}><i className="material-icons">wifi</i> WiFi</p>
+            <p className={features.balcony ? '' : 'dontExist'}><i className="material-icons">straighten</i> Balcony</p>
+            <p className={features.iron ? '' : 'dontExist'}><i className="material-icons">scanner</i> Iron</p>
+            <p className={features.pool ? '' : 'dontExist'}><i className="material-icons">pool</i> Pool</p>
+            <p className={features.fridge ? '' : 'dontExist'}><i className="material-icons">kitchen</i> Fridge</p>
+            <p className={features.dishwasher ? '' : 'dontExist'}><i className="material-icons">kitchen</i> Dishwasher</p>
+          </div>}
+        </div>
+        <div className="dates">
+          <DatePicker className="startDate"
+            placeholderText="Arrival.."
+            selected={startDate}
+            onChange={date => setStartDate(date)}
+            minDate={residence.startDate * 1000}
+            maxDate={residence.endDate * 1000}
+            filterDate={filterForStartDate}
+            isClearable
           />
-      </div>
-      {totalPrice && <p><span>Total price: </span>{totalPrice} €</p>}
-      {unFilledFields && <p className="valCheck">You have to pick a start date and a end date to continue..</p>}
-      <button onClick={bookResidence} class="book-btn">Book</button>
+          <DatePicker className="endDate"
+            placeholderText="Departure.."
+            selected={endDate}
+            onChange={date => setEndDate(date)}
+            minDate={residence.startDate * 1000}
+            maxDate={residence.endDate * 1000}
+            filterDate={filterForEndDate}
+            isClearable
+          />
+        </div>
+        {totalPrice && <p><span>Total price: </span>{totalPrice} €</p>}
+        {unFilledFields && <p className="valCheck">You have to pick a start date and a end date to continue..</p>}
+        <button onClick={bookResidence} class="book-btn">Book</button>
+      </div>}
+      {showConfirmPage && <div className="confirm">
+        <h1>Thank you.</h1>
+        <h4>Your booked residence was completed succsessfully.</h4>
+        <img src={residence.imageURLs[0]} alt="" />
+        <p><span>Address: </span>{ residence.address }</p>
+        <p><span>Start date: </span>{new Date(startDate).toString().substr(0,15)}</p>
+        <p><span>End date: </span>{new Date(endDate).toString().substr(0, 15)}</p>
+        <p><span>Total price: </span>{totalPrice} €</p>
+      </div>}
     </div>
   );
 }
