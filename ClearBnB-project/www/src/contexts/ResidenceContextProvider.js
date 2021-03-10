30812874f964c4ca9135f80c6461fc2067a82250
oscargregorius <oscar.gregorius@gmail.com>
2021-03-10 11:00:44 +0100

    Updateted the put method.

--- ClearBnB-project/www/src/contexts/ResidenceContextProvider.js
+++ ClearBnB-project/www/src/contexts/ResidenceContextProvider.js
@@ -13,14 +13,14 @@ export const ResidenceProvider = (props) => {
     setResidences([...data]);
   }
 
-  const updateResidence = async (id) => {
-    //  let res = await fetch('/rest/residences/' + id, {
-    //   method: 'PUT',
-    //   headers: {'content-type': 'application/json'},
-    //   body: JSON.stringify(recipe)
-    //  })
+  const updateResidence = async (id, residence) => {
+     let res = await fetch('/rest/residences/' + id, {
+      method: 'PUT',
+      headers: {'content-type': 'application/json'},
+      body: JSON.stringify(residence)
+     })
     
-    // res = await res.json()
+    res = await res.json()
   }
 
   useEffect(() => {
