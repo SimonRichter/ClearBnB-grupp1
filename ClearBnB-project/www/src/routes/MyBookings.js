0384c181bd14fbcbd3e86043ec25913af852d573
oscargregorius <oscar.gregorius@gmail.com>
2021-03-11 14:35:12 +0100

    imported all context values needed

--- ClearBnB-project/www/src/routes/MyBookings.js
+++ ClearBnB-project/www/src/routes/MyBookings.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ResidenceContext } from '../contexts/ResidenceContextProvider';
+import { BookingContext } from '../contexts/BookingContextProvider'
+import { UserContext } from '../contexts/UserContextProvider'
 
 const MyBookings = () => {
+
+  const { residences } = useContext(ResidenceContext);
+  const { bookings } = useContext(BookingContext);
+  const { whoAmI } = useContext(UserContext);
+
   return (
     <div className="myBookings">
-
+      <h1>My bookings..</h1>
     </div>
   );
 }
