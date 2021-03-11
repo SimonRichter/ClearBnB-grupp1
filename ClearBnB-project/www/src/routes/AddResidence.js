3f11eedfaa8de76613762f0b5f841f4667689f98
Ali94160 <ali.al-mousawi@hotmail.com>
2021-03-11 15:48:35 +0100

    Update Add residence
    Since every checkbox is uniqe we have to have uniqe useState variables and refs.

--- ClearBnB-project/www/src/routes/AddResidence.js
+++ ClearBnB-project/www/src/routes/AddResidence.js
@@ -2,6 +2,7 @@ import React, { useState, useRef, useEffect }from 'react';
 import '../style/AddResidenceStyle.css';
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
+import Login from './Login';
 
 
 
@@ -10,20 +11,8 @@ const AddResidence = () => {
   const [selectedStartDate, setSelectedStartDate] = useState(null);
   const [selectedEndDate, setSelectedEndDate] = useState(null);
   const [guests, setGuests] = useState(1);
-  const [featureState, setFeature] = useState([]);
-
-
-  useEffect(() => {
-    let featureState = [
-      { id: 1, name: 'firstAidKit' },
-      { id: 2, name: 'Shower' },
-      { id: 3, name: 'Parking'}
-    ]
-    
-    setFeature(featureState.map((f) => {
-      return{"select": false, name: f.name}
-    }))
-  }, []);
+  const [isChecked, setIsChecked] = useState(false);
+   const [isChecked2, setIsChecked2] = useState(false);
 
   const optType = useRef(null);
   const adTitle = useRef(null);
@@ -37,8 +26,21 @@ const AddResidence = () => {
   const imageRef3 = useRef(null);
   const imageRef4 = useRef(null);
   const imageRef5 = useRef(null);
-
-  
+    
+    const feature1 = useRef(false);
+    const feature2 = useRef(false);
+    const feature3 = useRef(false);
+    const feature4 = useRef(false);
+    const feature5= useRef(false);
+    const feature6 = useRef(false);
+    const feature7 = useRef(false);
+    const feature8 = useRef(false);
+    const feature9 = useRef(false);
+    const feature10 = useRef(false);
+    const feature11 = useRef(false);
+    const feature12 = useRef(false);
+    const feature13 = useRef(false);
+    const feature14 = useRef(false);
 
   const incGuestHandler = (e) => {
     e.preventDefault();
@@ -86,12 +88,41 @@ const AddResidence = () => {
       startDate: theStartDate,
       endDate: theEndDate
     }
-   
-    
     
    
     console.log(residence);
+
+    const features = {
+      firstAidKit: feature1.current.value,
+      shower: feature2.current.value
+    }
+    console.log("object", features);
   }
+
+  const addFeatureHandler = (e) => {
+   if (isChecked === false) {
+      setIsChecked(true)
+    } 
+    if (isChecked === true) {
+      setIsChecked(false);
+    }
+   console.log("on click", e.target.value);
+  }
+
+  const addFeatureHandler2 = (e) => {
+    if (isChecked2 === false) {
+      setIsChecked2(true)
+    } 
+    if (isChecked2 === true) {
+      setIsChecked2(false);
+  }
+   console.log("on click", e.target.value);
+  }
+
+
+
+
+
 
   return (
     <div className="addResWrapper">
@@ -120,62 +151,49 @@ const AddResidence = () => {
 
       
      
-        {/* <label key={f.id}>
-          <input onChange={event => {
-            let checked = event.target.checked;
-            setFeature(featureState.map(data => {
-              if (f.id == data.id) {
-                data.select = true;
-              }
-              return data;
-            }
-          }} type = "checkbox" checked = { f.select } /> <i className="helper" ></i>
-        
-        </label> */}
         <div className="checkbox">
 
-
         <label>
-            <input type="checkbox"/><i className="helper" ></i>First-Aid Kit
+            <input type="checkbox" onClick={addFeatureHandler} ref={feature1} value={isChecked}  /><i className="helper" ></i>First-Aid Kit
         </label>
           <label>
-            <input type="checkbox"/><i className="helper" ></i>Shower
+            <input type="checkbox" onClick={addFeatureHandler2} ref={feature2} value={isChecked2} /><i className="helper" ></i>Shower
         </label>
           <label>
-            <input type="checkbox"/><i className="helper" ></i>Parking
+            <input type="checkbox" onClick={addFeatureHandler} ref={feature3} value="Parking" /><i className="helper" ></i>Parking
         </label>
           <label>
-            <input type="checkbox" /><i className="helper"></i>Stove
+            <input type="checkbox" onClick={addFeatureHandler} ref={feature4} value="Stove" /><i className="helper"></i>Stove
         </label>
           <label>
-            <input type="checkbox" /><i className="helper"></i>Oven
+            <input type="checkbox" onClick={addFeatureHandler} ref={feature5} value="Oven" /><i className="helper"></i>Oven
         </label>
           <label>
-            <input type="checkbox" /><i className="helper"></i>Microwave
+            <input type="checkbox" onClick={addFeatureHandler} ref={feature6} value="Microwave" /><i className="helper"></i>Microwave
         </label>
           <label>
-            <input type="checkbox" /><i className="helper"></i>Tv
+            <input type="checkbox" onClick={addFeatureHandler} ref={feature7} value="TV" /><i className="helper"></i>Tv
         </label>
           <label>
-            <input type="checkbox" /><i className="helper"></i>Coffee maker
+            <input type="checkbox" onClick={addFeatureHandler} ref={feature8} value="Coffe maker" /><i className="helper"></i>Coffee maker
         </label>
           <label>
-            <input type="checkbox" /><i className="helper"></i>WIFI
+            <input type="checkbox" onClick={addFeatureHandler} ref={feature9} value="WIFI" /><i className="helper"></i>WIFI
         </label>
           <label>
-            <input type="checkbox" /><i className="helper"></i>Balcony
+            <input type="checkbox" onClick={addFeatureHandler} ref={feature10} value="Balcony" /><i className="helper"></i>Balcony
         </label>
           <label>
-            <input type="checkbox" /><i className="helper"></i>Iron
+            <input type="checkbox" onClick={addFeatureHandler} ref={feature11} value="Iron" /><i className="helper"></i>Iron
         </label>
           <label>
-            <input type="checkbox" /><i className="helper"></i>Pool
+            <input type="checkbox" onClick={addFeatureHandler} ref={feature12} value="Pool" /><i className="helper"></i>Pool
         </label>
           <label>
-            <input type="checkbox" /><i className="helper"></i>Fridge
+            <input type="checkbox" onClick={addFeatureHandler} ref={feature13} value="Fridge" /><i className="helper"></i>Fridge
         </label>
           <label>
-            <input type="checkbox" /><i className="helper"></i>Dishwasher
+            <input type="checkbox" onClick={addFeatureHandler} ref={feature14} value="Dishwasher" /><i className="helper"></i>Dishwasher
         </label>
         </div>
         
