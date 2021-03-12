23d441225f5189947fd3e89a7ad455ba38371cf7
SimonRichter <simonmrichter@gmail.com>
2021-03-12 08:44:37 +0100

    Dropdown menu functionality
    Co-Authored-By: Jonte111 <72066499+Jonte111@users.noreply.github.com>

--- ClearBnB-project/www/src/components/Nav.js
+++ ClearBnB-project/www/src/components/Nav.js
@@ -1,11 +1,24 @@
 import React from 'react'
 import '../style/Nav.css';
 import {Link} from 'react-router-dom'
+import { Dropdown, NavItem, NavLink } from 'react-bootstrap'
+
 
 const Nav = () => {
 
   return (
     <div className="nav-bar">
+      <Dropdown as={NavItem}>
+        <Dropdown.Toggle as={NavLink}>Hamburger Menu</Dropdown.Toggle>
+        <Dropdown.Menu>
+          <Dropdown.Item><Link to="/">Home</Link></Dropdown.Item>
+          <Dropdown.Item><Link to="/about">About</Link></Dropdown.Item>
+          <Dropdown.Item><Link to="/Residences">Residences</Link></Dropdown.Item>
+          <Dropdown.Item><Link to="/login">Login</Link></Dropdown.Item>
+          <Dropdown.Item><Link to="/register">Register</Link></Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>;
+
       <h4>ClearBnB</h4>
       <div className="links">
       <Link to="/">Home</Link>
