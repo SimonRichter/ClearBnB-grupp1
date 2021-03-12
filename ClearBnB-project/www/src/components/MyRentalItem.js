a9bc9fc827f0a1f52922422068b778568927025f
oscargregorius <oscar.gregorius@gmail.com>
2021-03-12 15:20:30 +0100

    Prints out all information needed
    Need CSS.

--- ClearBnB-project/www/src/components/MyRentalItem.js
+++ ClearBnB-project/www/src/components/MyRentalItem.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import '../style/MyRentalItem.css'
 
-const MyRentalItem = ({rental}) => {
+const MyRentalItem = ({ rental }) => {
+  
+  const startDate = rental.startDate * 1000;
+  const endDate = rental.endDate * 1000;
+
+
   return (
     <div className="myRentalItem">
-      <p>{rental.title}</p>
+      <div className="img">
+        <img src={rental.imageURLs[0]} alt=""/>
+      </div>
+      <div className="info">
+        <p><span>Title: </span>{rental.title}</p>
+        <p><span>Start date: </span>{new Date(startDate).toString().substr(0, 15)}</p>
+        <p><span>End date: </span>{new Date(endDate).toString().substr(0, 15)}</p>
+        <p><span>Price/night: </span>{ rental.price }</p>
+      </div>
     </div>
   );
 }
