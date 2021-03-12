e4ac0548f68085806899ba2bbbaa0d7f145c0b45
oscargregorius <oscar.gregorius@gmail.com>
2021-03-12 14:35:12 +0100

    Made a function
    That filter out your rentals.

--- ClearBnB-project/www/src/routes/MyRentals.js
+++ ClearBnB-project/www/src/routes/MyRentals.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { ResidenceContext } from '../contexts/ResidenceContextProvider';
+import { UserContext } from '../contexts/UserContextProvider'
 
 const MyRentals = () => {
+
+  const { whoAmI } = useContext(UserContext);
+  const { residences } = useContext(ResidenceContext);
+
+  const [rentals, setRentals] = useState(null);
+
+  useEffect(() => {
+    const myRentals = residences.filter(r => r.userId === whoAmI._id);
+    console.log('id', whoAmI._id);
+    console.log('correct',myRentals);
+  },[])
+
   return (
     <div className="myRentals">
       <h1>My rentals</h1>
