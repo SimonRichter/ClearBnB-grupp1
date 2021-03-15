5117dbc5ba741d62bfe3cdb538588dbc8978146b
Jonte111 <jonathan.pe@telia.com>
2021-03-15 10:12:37 +0100

    Works but needs styling.

--- ClearBnB-project/www/src/routes/page404.js
+++ ClearBnB-project/www/src/routes/page404.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import '../style/About.css'
 
-const Page404 = () =>{
+const Page404 = () => {
+  return (
   <div className="page404">
-
+     <h1>Page does not exist {window.location.pathname}</h1>
   </div>
+  )
 }
 export default Page404;
