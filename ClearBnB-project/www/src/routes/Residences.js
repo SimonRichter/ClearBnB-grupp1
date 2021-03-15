8370260a0293327dd1cf9c2a1a86e28c9787cb35
oscargregorius <oscar.gregorius@gmail.com>
2021-03-15 14:48:30 +0100

    Fixed a key "problem"

--- ClearBnB-project/www/src/routes/Residences.js
+++ ClearBnB-project/www/src/routes/Residences.js
@@ -67,7 +67,6 @@ const Residences = (props) => {
       <button onClick={showAll}>Show all</button>
       </div>
       {filteredList && <ResidenceList residences={filteredList} />}
-      {console.log('times')}
     </div>
   );
 }
