7f286c1f047ac02312a80758dac9ea17a344a5b1
oscargregorius <oscar.gregorius@gmail.com>
2021-03-15 16:11:00 +0100

    Added logic for the open and close search.

--- ClearBnB-project/www/src/routes/Residences.js
+++ ClearBnB-project/www/src/routes/Residences.js
@@ -56,6 +56,7 @@ const Residences = (props) => {
     <div className="residences">
       {!showSearchFields && <button onClick={() => setShowSearchFields(true)}>SEARCH 🔍</button>}
       {showSearchFields && <div className="searchFields">
+        <p onClick={() => setShowSearchFields(false)}>X</p>
         <form onSubmit={searchFor}>
           <input ref={country} type="text" placeholder="Search by country.." />
           <input ref={city} type="text" placeholder="Search by city.." />
