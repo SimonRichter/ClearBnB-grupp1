49c91a09f7059f18b46b389db19f86dd75d5b3a6
Ali94160 <ali.al-mousawi@hotmail.com>
2021-03-16 15:04:18 +0100

    Revert "Merge branch 'develop' of https://github.com/Jonte111/ClearBnB-grupp1 into develop"
    This reverts commit ab7dfd2972ee49afd2dfcbc1300457b80f342e84, reversing
    changes made to b29c19707d600b3d3e52c422763356bff3cc8f15.

--- ClearBnB-project/www/src/components/Nav.js
+++ ClearBnB-project/www/src/components/Nav.js
@@ -1,49 +1,31 @@
-import React, { useState, useContext, useEffect } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { UserContext } from '../contexts/UserContextProvider'
 import '../style/Nav.css';
-import {Link} from 'react-router-dom'
-import { NavItem } from 'react-bootstrap'
-import ProfileMenu from './ProfileMenu.js'
+import { Link } from 'react-router-dom'
+import ProfileMenu from './ProfileMenu';
+// import {NavItem} from 'react-bootstrap'
 
 
 const Nav = () => {
 
-  const { whoAmI, logOut, whoIsOnline } = useContext(UserContext);
+const { whoAmI, whoIsOnline } = useContext(UserContext);
   
-
   useEffect(() => {
     whoIsOnline();
-  }, [])
-  
-  const [isActive, setActive] = useState("false");
-
-  const handleToggle = () => {
-    setActive(!isActive);
-  };
+  },[])
 
   return (
-    <div className={isActive ? "nav-bar shadow" : "nav-bar no-shadow"}>
-      <div as={NavItem} className="hamburger-menu-opened">
-        <p className="hamburger-menu" onClick={handleToggle}>☰</p>
-        <div className={isActive ? "display-none" : "flex-column"}>
-          <Link to="/" >Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/Residences">Residences</Link>
-          {!whoAmI && <Link to="/login">Login</Link>}
-          {!whoAmI && <Link to="/register">Register</Link>}
-          {whoAmI && <Link to="/" onClick={() => logOut()}>Log out</Link>}
-        </div>
-      </div>
+    <div className="nav-bar">
      <Link to="/"><img className="logoImg" src="https://i.postimg.cc/020TTsWC/logo-transparent-2.png" alt=""/></Link>
       <div className="links">
       <Link to="/">HOME</Link>
       <Link to="/about">ABOUT</Link>
-      <Link to="/Residences">RESIDENCES</Link>
+        <Link to="/Residences">RESIDENCES</Link>
       {!whoAmI && <Link to="/login">LOGIN</Link>}
       {!whoAmI && <Link to="/register">REGISTER</Link>} 
+         <ProfileMenu/>
       </div>
-      <ProfileMenu className="profileMenu"/>
-      </div>
+    </div>
   ); 
 }
 
