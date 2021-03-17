78e7baef5aa3230b9644b20497c227a872af6bbc
oscargregorius <oscar.gregorius@gmail.com>
2021-03-17 10:01:05 +0100

    Updateted the persons object.

--- ClearBnB-project/www/src/components/Person.js
+++ ClearBnB-project/www/src/components/Person.js
@@ -1,9 +1,21 @@
 import React from 'react'
 
-const Person = () => {
+const Person = ({person}) => {
   return (
     <div className="person">
-      
+    <div className="flip-card">
+    <div class="flip-card-inner">
+      <div class="flip-card-front">
+          <img src="https://cdn.discordapp.com/attachments/815586876261531659/821391699459702804/1581419371598.jpg" alt="" />
+            <p>{person.name}</p>
+      </div>
+        <div class="flip-card-back">
+            <p><span>NAME: </span>{person.name}</p>
+            <p><span>NR: </span>{person.nr}</p>
+            <p><span>EMAIL: </span>{person.email}</p>
+        </div>
+    </div>
+    </div>
     </div>
   );
 }
--- ClearBnB-project/www/src/routes/About.js
+++ ClearBnB-project/www/src/routes/About.js
@@ -5,10 +5,30 @@ import Person from '../components/Person'
 const About = () => {
 
   const persons = [
-    { name: "Oscar Gregorius", nr: "0728492302", email: "oscar@clearbnb.se" },
-    { name: "Ali Al-Mousawi", nr: "0728492302", email: "ali@clearbnb.se" },
-    { name: "Jonathan Persson", nr: "0728492302", email: "jonathan@clearbnb.se" },
-    { name: "Simon Richter", nr: "0728492302", email: "simon@clearbnb.se" }
+    {
+      name: "Oscar Gregorius",
+      nr: "0728492302",
+      email: "oscar@clearbnb.se",
+      img: "https://cdn.discordapp.com/attachments/815586876261531659/821390241982054510/image0.png"
+    },
+    {
+      name: "Ali Al-Mousawi",
+      nr: "0728492302",
+      email: "ali@clearbnb.se",
+      img: "https://cdn.discordapp.com/attachments/815586876261531659/821391528063926282/IMG_20200817_132848_332.jpg"
+    },
+    {
+      name: "Jonathan Persson",
+      nr: "0728492302",
+      email: "jonathan@clearbnb.se",
+      img: "https://cdn.discordapp.com/attachments/815586876261531659/821393083659845692/IMG_20210316_153701.jpg"
+    },
+    {
+      name: "Simon Richter",
+      nr: "0728492302",
+      email: "simon@clearbnb.se",
+      img: "https://cdn.discordapp.com/attachments/815586876261531659/821391699459702804/1581419371598.jpg"
+    }
   ];
 
 
