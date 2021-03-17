c73707eb75c8adf2e0a2b6395015589e9da4e9a4
oscargregorius <oscar.gregorius@gmail.com>
2021-03-17 13:27:08 +0100

    Added some text in about. need some css.

--- ClearBnB-project/www/src/routes/About.js
+++ ClearBnB-project/www/src/routes/About.js
@@ -35,18 +35,17 @@ const About = () => {
 
   return (
     <div className="about">
-      <div className="outerContainer">
-        <div className="grid-container">
-          <div className="grid-item"></div>
-          <div className="grid-item"></div>
-          <div className="grid-item"></div>
-          <div className="grid-item"></div>
-          <div className="grid-item-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero consectetur aliquid ab excepturi possimus fugit animi porro et. Odit ipsam excepturi aspernatur tempora at quis et fuga expedita cupiditate voluptates.</div>
-          <div className="grid-item"></div>
-          <div className="grid-item"></div>
-          <div className="grid-item-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad doloremque blanditiis maiores eveniet, in nesciunt porro rerum id facilis laborum ex iusto nemo quaerat? Cupiditate sequi a reprehenderit fugit blanditiis!</div>
-          <div className="grid-item"></div>
-        </div>
+      <div className="information">
+          <p className="head">About ClearBnB</p>
+          <p>ClearBnB was created on 16:th November 2007 by two young university students in their garage.
+            The company stands for equal rights and have enrolled a diverse staff that helps us to create an environment where everyone can feel welcome and included.
+            We also compensate for our CO2 emissions to 100% by planting trees in Malaysia and help conserving freshwater usage by instead using salt water to cool our servers.</p>
+          <p>We believe that everyone should have access to shelter; wether they are on travels, are fleeing a war or have had their homes destroyed by a natural disaster.
+            Therefore we donate 15% of our earnings to the Safe Home Association that helps people in need with housing.</p>
+          <p>
+            We are a steadily growing company with shares on the stock market. We hope you have a good experience using our service, and would appreciate it very much if you
+            refer us to your friends and family. 
+          </p>
       </div>
       <div className="persons">
         {persons.map(p => (<Person person={p} />))}
