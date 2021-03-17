ccef4f2a0939ecd99a22b71246c3b53b7829c658
oscargregorius <oscar.gregorius@gmail.com>
2021-03-17 09:56:44 +0100

    Created an array with objects
    that holds each person in the company.

--- ClearBnB-project/www/src/routes/About.js
+++ ClearBnB-project/www/src/routes/About.js
@@ -1,23 +1,21 @@
 import React from 'react'
 import '../style/About.css'
+import Person from '../components/Person'
 
 const About = () => {
+
+  const persons = [
+    { name: "Oscar Gregorius", nr: "0728492302", email: "oscar@clearbnb.se" },
+    { name: "Ali Al-Mousawi", nr: "0728492302", email: "ali@clearbnb.se" },
+    { name: "Jonathan Persson", nr: "0728492302", email: "jonathan@clearbnb.se" },
+    { name: "Simon Richter", nr: "0728492302", email: "simon@clearbnb.se" }
+  ];
+
+
+
   return (
     <div className="about">
       <div className="outerContainer">
-
-        {/* <div className ="title">
-          <h1>About page</h1>
-        </div>
-
-        <div className="upperDescription">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta autem illo ullam sunt reprehenderit quisquam! Numquam eligendi recusandae natus repellat asperiores, excepturi adipisci veniam illo nesciunt cumque at ipsam a.
-        </div>
-
-        <div className="lowerDescription">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam minima assumenda, pariatur at ab illo architecto cupiditate sit iure, explicabo ullam asperiores dolorem reiciendis reprehenderit nesciunt similique eaque quas dolor?
-        </div> */}
-        {/* ----------------------------------------Grid test */}
         <div className="grid-container">
           <div className="grid-item"></div>
           <div className="grid-item"></div>
