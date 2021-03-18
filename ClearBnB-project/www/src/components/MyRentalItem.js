ab7639164c75d376b6b495e76f3e2f87c1cdfb2f
oscargregorius <oscar.gregorius@gmail.com>
2021-03-18 15:36:42 +0100

    Added a delete function that will delete the residence.
    Co-Authored-By: Ali <72066097+ali94160@users.noreply.github.com>

--- ClearBnB-project/www/src/components/MyRentalItem.js
+++ ClearBnB-project/www/src/components/MyRentalItem.js
@@ -8,20 +8,24 @@ const MyRentalItem = ({ rental }) => {
   const startDate = rental.startDate * 1000;
   const endDate = rental.endDate * 1000;
 
+  const deleteResidence = () => {
+    console.log(rental._id);
+  }
+
 
   return (
-    <div className="myRentalItem" onClick={() => history.push("/residence/" + rental._id)}>
+    <div className="myRentalItem">
       <div className="img">
         <img src={rental.imageURLs[0]} alt=""/>
       </div>
-      <div className="info">
+      <div className="info" onClick={() => history.push("/residence/" + rental._id)}>
         <p><span>Title: </span>{rental.title}</p>
         <p><span>Start date: </span>{new Date(startDate).toString().substr(0, 15)}</p>
         <p><span>End date: </span>{new Date(endDate).toString().substr(0, 15)}</p>
         <p><span>Price/night: </span>{ rental.price }</p>
       </div>
       <div className="delete">
-        <p className="deleteRes">❌</p>
+        <p className="deleteRes" onClick={deleteResidence}>❌</p>
       </div>
     </div>
   );
