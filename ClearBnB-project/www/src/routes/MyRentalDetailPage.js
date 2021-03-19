cb1e874f2c37030088446aef86e7fc1bd1e9aec7
oscargregorius <oscar.gregorius@gmail.com>
2021-03-19 08:29:15 +0100

    Created the MyRentalDetailPage component

---
+++ ClearBnB-project/www/src/routes/MyRentalDetailPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyRentalDetailPage = () => {
+  return (
+    <div className="myRentalDetailPage">
+      
+    </div>
+  );
+}
+
+export default MyRentalDetailPage;
