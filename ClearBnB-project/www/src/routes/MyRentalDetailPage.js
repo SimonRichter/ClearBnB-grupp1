f0406c1411147a34d6a18f8c75cfe340f5d2e310
oscargregorius <oscar.gregorius@gmail.com>
2021-03-19 09:28:42 +0100

    Added information about the residence.
    Co-Authored-By: Ali <72066097+ali94160@users.noreply.github.com>

--- ClearBnB-project/www/src/routes/MyRentalDetailPage.js
+++ ClearBnB-project/www/src/routes/MyRentalDetailPage.js
@@ -10,7 +10,23 @@ const MyRentalDetailPage = () => {
 
   return (
     <div className="myRentalDetailPage">
-      <h1>Rental detail {residence.title}</h1>
+      <div className="images">
+          {residence.imageURLs.map((img) => {
+            return (<img key={img} src={img} alt="" />)
+          })}
+        </div>
+        <div className="infoWrapper">
+          <div className="desc">
+            <p className="resTitle">{residence.title}</p>
+            <p><span>Country: </span>{residence.country}</p>
+            <p><span>City: </span>{residence.city}</p>
+            <p><span>Address: </span>{residence.address}</p>
+            <p><span>Type: </span>{residence.type}</p>
+            <p><span>Price per night: </span>{residence.price}€</p>
+            <p><span>Residence limit: </span>{residence.residenceLimit}</p>
+            <p><span>Description: </span>{residence.description}</p>
+        </div>
+        </div>
     </div>
   );
 }
