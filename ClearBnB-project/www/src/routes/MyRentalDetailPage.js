00572c1fd467abd107905bff6203397964299127
oscargregorius <oscar.gregorius@gmail.com>
2021-03-19 13:39:30 +0100

    Added a custom diagram with the stats.
    Co-Authored-By: Ali <72066097+ali94160@users.noreply.github.com>

--- ClearBnB-project/www/src/routes/MyRentalDetailPage.js
+++ ClearBnB-project/www/src/routes/MyRentalDetailPage.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { ResidenceContext } from '../contexts/ResidenceContextProvider';
 import { useParams, useHistory } from 'react-router-dom'
 import '../style/MyRentalDetail.css'
@@ -6,43 +6,66 @@ import '../style/MyRentalDetail.css'
 const MyRentalDetailPage = () => {
 
   const { id } = useParams();
-  const { residences, updateResidence } = useContext(ResidenceContext);
+  const { residences } = useContext(ResidenceContext);
   let residence = residences.find(r => r._id === id);
-  let percentOfBookings = (residence.amountOfBookings / residence.views) * 100
+  const [percentOfBookings, setPercentOfBookings] = useState(null);
+
+  useEffect(() => {
+    if (residence) {
+      setPercentOfBookings((residence.amountOfBookings / residence.views) * 100);
+    }
+  },[residences])
+
+  const styleView = {
+    height: residence.views + "%",
+    maxHeight: "100%"
+  }
+
+   const styleBookedDays = {
+    height: residence.bookedDays + "%",
+    maxHeight: "100%"
+  }
 
   return (
     <div className="myRentalDetailPage">
-          <div className="images">
-          {residence.imageURLs.map((img) => {
-            return (<img key={img} src={img} alt="" />)
-          })}
-        </div>
-        <div className="inner">
+      {residence && <div className="images">
+        {residence.imageURLs.map((img) => {
+          return (<img key={img} src={img} alt="" />)
+        })}
+      </div>}
+      {residence && <div className="inner">
         <div className="information">
-        <div className="infoWrapper">
-          <div className="desc">
-            <p className="resTitle">{residence.title}</p>
-            <p><span>Country: </span>{residence.country}</p>
-            <p><span>City: </span>{residence.city}</p>
-            <p><span>Address: </span>{residence.address}</p>
-            <p><span>Type: </span>{residence.type}</p>
-            <p><span>Price per night: </span>{residence.price}€</p>
-            <p><span>Residence limit: </span>{residence.residenceLimit}</p>
-            <p><span>Description: </span>{residence.description}</p>
+          <div className="infoWrapper">
+            <div className="desc">
+              <p className="resTitle">{residence.title}</p>
+              <p><span>Country: </span>{residence.country}</p>
+              <p><span>City: </span>{residence.city}</p>
+              <p><span>Address: </span>{residence.address}</p>
+              <p><span>Type: </span>{residence.type}</p>
+              <p><span>Price per night: </span>{residence.price}€</p>
+              <p><span>Residence limit: </span>{residence.residenceLimit}</p>
+              <p><span>Description: </span>{residence.description}</p>
+            </div>
           </div>
         </div>
-      </div>
         <div className="statistics">
-        <p className="statisticsTitle">statistics</p>
+          <div className="statInfo">
+          <p className="statisticsTitle">statistics</p>
           <p><span>Total booked days: </span>{residence.bookedDays.length}</p>
           <p><span>Pageviews: </span>{residence.views}</p>
           <p><span>Amount of bookings: </span>{residence.amountOfBookings}</p>
           <p><span>Total earnings: </span>{residence.earned}</p>
-          <p><span>Bookings procentage by pageviews: </span>{percentOfBookings.toFixed(2)}%</p>
-      </div>
-      </div>
+          {percentOfBookings && <p><span>Bookings procentage by pageviews: </span>{percentOfBookings.toFixed(2)}%</p>}
+          </div>
+          <div className="stats">
+            <div style={styleView} className="statViews"></div>
+            <div style={styleBookedDays} className="statBooked"></div>
+        </div>
+        </div>
+      </div>}
     </div>
   );
 }
+
 
 export default MyRentalDetailPage;
